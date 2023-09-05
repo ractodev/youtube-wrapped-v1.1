52e4254480ba78f5d65db05a893247db46563d06
ractodev <erik.j.winbladh@gmail.com>
2023-09-05 22:24:47 +0200

    Add Video class.

--- services/data-fetcher/src/main/java/com/ractodev/youtubewrapped/datafetcher/model/TakeoutProcessingService.java
+++ services/data-fetcher/src/main/java/com/ractodev/youtubewrapped/datafetcher/model/TakeoutProcessingService.java
@@ -2,4 +2,33 @@ package com.ractodev.youtubewrapped.datafetcher.model;
 
 // Reads takeout file, parses to user and video objects, fetches additional details from youtube api
 public class TakeoutProcessingService {
+    private String filePath;
+
+    // Method that initializes data processing, and returns true/false if processing was successful
+    public void processTakeoutData(String filePath) {
+        this.filePath = filePath;
+        readTakeoutFile();
+    }
+
+    // Reads the Google Takeout JSON file
+    private void readTakeoutFile() {
+
+    }
+
+    /*
+    // Parses the raw data to extract relevant details returned by readTakeoutFile()
+    private void parseWatchHistory(Object rawData) {
+
+    }
+
+    // Fetches additional details (like video length) for a given video
+    private void fetchAdditionalVideoDetails(Video video) {
+
+    }
+
+    // Saves the parsed watch history to database
+    private void saveToDatabase(List<Video> video) {
+
+    }
+    */
 }
--- services/data-fetcher/src/main/java/com/ractodev/youtubewrapped/datafetcher/service/Video.java
+++ services/data-fetcher/src/main/java/com/ractodev/youtubewrapped/datafetcher/service/Video.java
@@ -1,4 +1,38 @@
 package com.ractodev.youtubewrapped.datafetcher.service;
 
 public class Video {
+    private String title;
+    private String creator;
+    private String url;
+    private String category;
+    private double length;
+
+    // Constructor
+    public Video(String title, String creator, String url, String category, double length) {
+        this.title = title;
+        this.creator = creator;
+        this.url = url;
+        this.category = category;
+        this.length = length;
+    }
+
+    public String getVideoTitle() {
+        return this.title;
+    }
+
+    public String getVideoCreator() {
+        return this.creator;
+    }
+
+    public String getVideoUrl() {
+        return this.url;
+    }
+
+    public String getVideoCategory() {
+        return this.category;
+    }
+
+    public Double getVideoLength() {
+        return this.length;
+    }
 }
