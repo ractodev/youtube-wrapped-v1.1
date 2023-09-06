2553e832fcfd9875abe59996ca08d74b5e7d8733
ractodev <erik.j.winbladh@gmail.com>
2023-09-06 08:35:33 +0200

    Add user class

--- services/data-fetcher/src/main/java/com/ractodev/youtubewrapped/datafetcher/service/User.java
+++ services/data-fetcher/src/main/java/com/ractodev/youtubewrapped/datafetcher/service/User.java
@@ -1,4 +1,22 @@
 package com.ractodev.youtubewrapped.datafetcher.service;
 
+import java.util.List;
+
 public class User {
+    private String userId;
+    private List<Video> watchedVideos;
+
+    // Constructor
+    public User(String userId, List<Video> watchedVideos) {
+        this.userId = userId;
+        this.watchedVideos = watchedVideos;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public List<Video> getWatchedVideos() {
+        return this.watchedVideos;
+    }
 }
